b1df7d071105e0c343778870fe0d6661f3a41e1f
K.C Dhananjaya Perera <dananjayaperera99@gmail.com>
2024-10-13 19:42:12 +0530

    update category route code

--- routes/category.Route.js
+++ routes/category.Route.js
@@ -4,13 +4,14 @@ import { createCategory, deleteCategory, getCategoryByName, viewCategory } from 
 const categoryRouter = express.Router()
 
 categoryRouter.post("/",createCategory)
+
 categoryRouter.get("/",viewCategory)
 
 categoryRouter.get("/:name",getCategoryByName)
 
-
 categoryRouter.delete("/:name",deleteCategory)  //jsone eke delete karanna one userwa ewanne nathuwa url ekema dala apuwama
                                 //url eke dala ewana uswer ganna vidiya
+                                //parese walin ena ewa meh file eke anthimata dagana . nattam error enne puluwan
     
 
 
