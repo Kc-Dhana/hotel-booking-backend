865ce7682c8eafbd66c266c74421a4e9ff074e9f
K.C Dhananjaya Perera <dananjayaperera99@gmail.com>
2024-10-13 15:56:25 +0530

    error fix

--- controllers/galleryItemController.js
+++ controllers/galleryItemController.js
@@ -1,23 +1,23 @@
-import GalleryItem from "../models/galleryItem.js" //.js dana one
+import GalleryItem from "../models/galleryItem.js"      //.js dana one
 
-//create gallery item
+                                                        //create gallery item
 export function createGalleryItem(req,res){
 
-    const user = req.body.user  //index.js eke midelware eken userwa ewanne req eke dala
-    if(user == null){    //user kenek nattam
+    const user = req.user                               //index.js eke midelware eken userwa ewanne req eke dala
+    if(user == null){                                   //user kenek nattam
         res.status(403).json({
             message : "Please login to create a gallery item"
         })
-        return //createGalleryItem fun eka methanin nawattanwa.nattam pallaha code run wenwa retrun damme nattam
+        return                                          //createGalleryItem fun eka methanin nawattanwa.nattam pallaha code run wenwa retrun damme nattam
     }
-    if(user.type != "admin"){  //user admin ekenk neme nam
+    if(user.type != "admin"){                           //user admin ekenk neme nam
         res.status(403).json({
             message : "you dont have permmission to create a gallery item"
         })
-        return //createGalleryItem fun eka methanin nawattanwa.nattam pallaha code run wenwa retrun damme nattam
+        return                                          //createGalleryItem fun eka methanin nawattanwa.nattam pallaha code run wenwa retrun damme nattam
     }
 
-    const galleryItem = req.body.item
+    const galleryItem = req.item
 
     const newGalleryItem = new GalleryItem(galleryItem)
     newGalleryItem.save().then(
