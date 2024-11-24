2d8c846478349431e5daf9918745001816a5aae8
K.C Dhananjaya Perera <dananjayaperera99@gmail.com>
2024-11-24 12:24:07 +0530

    addupdateCategory rouets

--- routes/category.Route.js
+++ routes/category.Route.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { createCategory, deleteCategory, getCategoryByName, viewCategory } from "../controllers/categoryController.js";
+import { createCategory, deleteCategory, getCategoryByName, updateCategory, viewCategory } from "../controllers/categoryController.js";
 
 const categoryRouter = express.Router()
 
@@ -12,6 +12,8 @@ categoryRouter.get("/:name",getCategoryByName)
 categoryRouter.delete("/:name",deleteCategory)  //jsone eke delete karanna one userwa ewanne nathuwa url ekema dala apuwama
                                 //url eke dala ewana uswer ganna vidiya
                                 //parese walin ena ewa meh file eke anthimata dagana . nattam error enne puluwan
+
+categoryRouter.put("/:name",updateCategory) //ctrl and space press karanwa import karana auto avilla thibbe nattam
     
 
 
