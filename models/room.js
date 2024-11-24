5bac7a77e7c5e451f150b391aebf276598c0a2d1
K.C Dhananjaya Perera <dananjayaperera99@gmail.com>
2024-11-24 12:50:52 +0530

    create room model

---
+++ models/room.js
@@ -0,0 +1,41 @@
+import mongoose, { Mongoose } from "mongoose";
+
+
+const roomSchema =mongoose.Schema({
+    roomId : {
+        type : Number,
+        required : true,
+        unique : true
+    },
+    category : {             //not relational like sql when woerking with mongofb
+        type : String,
+        required : true
+    },
+    maxGuests : {
+        type : Number,
+        required : true,
+        default:3
+    },
+    available : {
+        type : Boolean,
+        required : true,
+        default : true
+    },
+    photos :[
+        {
+            type : String     
+        }
+    ],
+    specialdescription : {
+        type : String,
+        default:""
+    },
+    notes : {
+        type : String,                  //passe mona hari chage unoth meka wenas kara ganna punwan nisa developeta lesi wenna
+        default:""
+    }
+})
+
+const Room =mongoose.model("Rooms",roomSchema)
+
+export default Room;
