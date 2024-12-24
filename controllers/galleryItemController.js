2f44dd9ea6dd32a4d0c7a80c823fab841354dc56
K.C Dhananjaya Perera <dananjayaperera99@gmail.com>
2024-12-24 22:11:43 +0530

    add updateGalleryItem and deleteGalleryItem funtion and routes

--- controllers/galleryItemController.js
+++ controllers/galleryItemController.js
@@ -17,7 +17,7 @@ export function createGalleryItem(req,res){
         return                                          //createGalleryItem fun eka methanin nawattanwa.nattam pallaha code run wenwa retrun damme nattam
     }
 
-    const galleryItem = req.item
+    const galleryItem = req.body
 
     const newGalleryItem = new GalleryItem(galleryItem)
     newGalleryItem.save().then(
@@ -41,5 +41,63 @@ export function getGalleryItem(req,res){
                 list : list
             })
         }
+    )
+}
+export function deleteGalleryItem(req,res){
+    const user = req.user                               //index.js eke midelware eken userwa ewanne req eke dala
+    if(user == null){                                   //user kenek nattam
+        res.status(403).json({
+            message : "Please login to create a gallery item"
+        })
+        return                                          //createGalleryItem fun eka methanin nawattanwa.nattam pallaha code run wenwa retrun damme nattam
+    }
+    if(user.type != "admin"){                           //user admin ekenk neme nam
+        res.status(403).json({
+            message : "you dont have permmission to create a gallery item"
+        })
+        return                                          //createGalleryItem fun eka methanin nawattanwa.nattam pallaha code run wenwa retrun damme nattam
+    }
+    const id = req.params.id
+    GalleryItem.findByIdAndDelete(id).then(
+        ()=>{
+            res.json({
+                message : "Gallery Item deleted successfully"
+            })
+        }
+    ).catch(
+        ()=>{
+            res.json({
+                message : "Gallery Item deletion failed"
+            })
+        }
+    )
+}
+export function updateGalleryItem(req,res){
+    const user = req.user                               //index.js eke midelware eken userwa ewanne req eke dala
+    if(user == null){                                   //user kenek nattam
+        res.status(403).json({
+            message : "Please login to create a gallery item"
+        })
+        return                                          //createGalleryItem fun eka methanin nawattanwa.nattam pallaha code run wenwa retrun damme nattam
+    }
+    if(user.type != "admin"){                           //user admin ekenk neme nam
+        res.status(403).json({
+            message : "you dont have permmission to create a gallery item"
+        })
+        return                                          //createGalleryItem fun eka methanin nawattanwa.nattam pallaha code run wenwa retrun damme nattam
+    }
+    const id = req.params.id
+    GalleryItem.findByIdAndUpdate(id,req.body).then(
+        ()=>{
+            res.json({
+                message : "Gallery Item updated successfully"
+            })
+        } 
+    ).catch(
+        ()=>{
+            res.json({ 
+                message : "Gallery Item updation failed"
+            })
+        }   
     )
 }
--- routes/galleryItemRoute.js
+++ routes/galleryItemRoute.js
@@ -1,9 +1,12 @@
 import express from "express";
-import {createGalleryItem, getGalleryItem} from "../controllers/galleryItemController.js"
+import {createGalleryItem, deleteGalleryItem, getGalleryItem, updateGalleryItem} from "../controllers/galleryItemController.js"
 
 const galleryItemRouter = express.Router()
 
 galleryItemRouter.post("/",createGalleryItem)
 galleryItemRouter.get("/",getGalleryItem)
+galleryItemRouter.put("/:id",updateGalleryItem)
+galleryItemRouter.delete("/:id",deleteGalleryItem)
+
 
 export default galleryItemRouter;
