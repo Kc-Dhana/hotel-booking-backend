7eaaec29b9c88f3a289cf583c64cb5131e7643fe
K.C Dhananjaya Perera <dananjayaperera99@gmail.com>
2025-02-17 18:21:51 +0530

    db quary part in create booking using catergory

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -1,4 +1,5 @@
 import Booking from "../models/booking.js";
+import Room from "../models/room.js";
 import { isCustomerValid } from "./userControllers.js";
 
 export function createBooking(req,res){
@@ -18,10 +19,10 @@ export function createBooking(req,res){
            const newId = startingId + count + 1;      //count eka(db eke recodes count) + karala startingId eka passe eka ganna new id ekata
            const newBooking = new Booking({
                bookingId : newId,
-               roomId : req.body.roomID,
+               roomId : req.body.roomId,
                email : req.user.email,
-               start : req.body.startDate,
-               end : req.body.endDate
+               start : req.body.start,
+               end : req.body.end
            });
            newBooking.save().then(
                (result) => {
@@ -95,4 +96,88 @@ export function retriveBookingByDate(req,res){
             });
         }
     )
+}
+
+export function createBookingUsingCategory(req,res){
+ 
+    const start = new Date(req.body.start);    //req eke tiyena stardate eka new start variable ekaka (date) vidiyata ganna
+    const end = new Date(req.body.end);
+
+    Booking.find({
+        $or :[        //condition 2k danawa. 2ken 1k hari giyath booked room ekka viyata ganne
+            {
+            start : {  //database eke start eka
+                $gte :start,               //cutomer dena eka //starting data eke kalin booking wala staring and end date atahra tiyenwada balanwa.
+                $lt :end                   //2ken 1k hari giyath booked room ekka viyata ganne
+                }
+            },
+            { 
+            end : {
+                $gte :start,
+                $lt :end
+                }
+            }
+        ]
+    }).then((response)=>{
+      const overlappingBookings = response;
+      const rooms = [];
+
+      for(let i=0; i<overlappingBookings.length; i++){ //ovverlap wena bookings ganwa me loop eka run wenwa . ekin eka
+        
+        rooms.push(overlappingBookings[i].roomId);   //overlapping booking eke roomId eka push karnwa rooms ekata arrya ekka vidiyata //book wela tiyana room id thama enne
+      }      
+
+      Room.find({                                //Room model eka import  karanna mulinma
+        roomId : {                                              
+            $in: rooms                            //rooms array eke(bookedroomid) nathi ithuru rooom id ganna vidiya
+        },
+        category : req.body.category              //e ena room id eken awashana categorye eke ewa vitharl filter karanwa 
+      }).then((room)=>{
+
+       if(room.length === 0){   
+        res.json({
+            message : "No rooms available",
+        });
+       }else{
+        const startingId = 1200; //auto genarate wena id eka 1000 patan ganna one nisa 
+
+        Booking.countDocuments({}).then(
+            (count) => {
+               console.log(count);
+               const newId = startingId + count + 1;      //count eka(db eke recodes count) + karala startingId eka passe eka ganna new id ekata
+               const newBooking = new Booking({
+                   bookingId : newId,
+                   roomId : room[0].roomId,             //filter wela apu mulma room eka ta thama booking eka vatenne
+                   email : req.user.email,
+                   start :start,                //date kalin variable ekata gatta nisa aye req eken ganne na direct variable eka gannwa
+                   end : end
+               });
+               newBooking.save().then(
+                   (result) => {
+                       res.json({
+                           message : "Booking created successfully",
+                           result : result
+                       });
+                   }
+               ).catch(
+                   (err) => {
+                       res.json({
+                           message : "Booking creation failed",
+                           error : err
+                       });
+                   }
+               );
+            }
+        ).catch(
+            (err) => {  
+                res.json({
+                    message : "Booking creation failed",
+                    error : err
+                })
+            }
+        )
+       }
+
+      })                         
+    })
 }
--- routes/bookingRoute.js
+++ routes/bookingRoute.js
@@ -1,11 +1,12 @@
 import express from "express";
-import getAllBookings, { createBooking, retriveBookingByDate } from "../controllers/bookingController.js";
+import getAllBookings, { createBooking, createBookingUsingCategory, retriveBookingByDate } from "../controllers/bookingController.js";
 
 const bookingRouter = express.Router();
 
 bookingRouter.post('/',createBooking);
 bookingRouter.get('/',getAllBookings);
 bookingRouter.post('/filter-date',retriveBookingByDate)
+bookingRouter.post('/create-by-category',createBookingUsingCategory)
 
 
 export default bookingRouter;
