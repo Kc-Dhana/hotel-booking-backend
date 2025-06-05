80d52f4b83d2de4f8da8f089e2b60d7691994063
K.C Dhananjaya Perera <dananjayaperera99@gmail.com>
2025-06-05 17:48:28 +0530

    add getAvailableRooms routes in booking

--- routes/bookingRoute.js
+++ routes/bookingRoute.js
@@ -1,12 +1,14 @@
 import express from "express";
-import getAllBookings, { createBooking, createBookingUsingCategory, retriveBookingByDate } from "../controllers/bookingController.js";
+import getAllBookings, { createBooking, createBookingUsingCategory, getAvailableRooms, retriveBookingByDate } from "../controllers/bookingController.js";
 
 const bookingRouter = express.Router();
 
 bookingRouter.post('/',createBooking);
 bookingRouter.get('/',getAllBookings);
+bookingRouter.get("/available", getAvailableRooms);
 bookingRouter.post('/filter-date',retriveBookingByDate)
 bookingRouter.post('/create-by-category',createBookingUsingCategory)
+
 
 
 export default bookingRouter;
